f566735eb8899e5c90913b291a9043fffd724d43
Oscar Giraldo <oscardan@utp.edu.co>
2020-12-02 14:55:56 -0500

    js main

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     convertirImgBase64();
 
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -61,7 +62,8 @@
     <body>
 
         <div id="contenedor">
-        <div class="row"> <div class="contFondo" id="contFondo"></div>
+        <div class="row"> 
+        <div class="contFondo" id="contFondo"></div>
             <div class="continfo">
                 <button type="button" id="boton" class="btn btn-light">activar camara</button>
                 <video autoplay id="video" class="camara"></video>
@@ -77,61 +79,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js"></script>
-        <script>
-
-            // =============================================================
-            // =============== Tomar foto y hacer Screenchot ===============
-            // =============================================================
-
-            const btnTomar = document.getElementById("btnTomarFoto");
-            btnTomar.addEventListener("click", () => {
-                const video = document.getElementById("video");
-                video.classList.add("hidden");
-                video.pause();
-
-                const canvas = document.getElementById('canvas');
-                const context = canvas.getContext('2d');
-                context.drawImage(video, 0, 0, 200, 150);
-                canvas.toDataURL('image/png');
-
-                html2canvas(document.body, {
-                    onrendered (canvas) {
-                        var link = document.createElement("a");
-                        var image = canvas.toDataURL();
-                        link.href = image;
-                        link.download = 'screenshot.png';
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-                    }
-                });
-            });
-
-            // ==============================================
-            // =============== Activar camara ===============
-            // ==============================================
-
-            let video = document.querySelector("#video");
-            document.querySelector("#boton").addEventListener("click", function(ev){
-                navigator.mediaDevices.getUserMedia({audio: false, video: true})
-                .then(record)
-                .catch(err => console.log(err));
-
-            })
-            function record(stream){
-                video.srcObject = stream;
-            }
-
-            // =======================================================
-            // =============== Convertir fondo a bas64 ===============
-            // =======================================================
-
-            window.addEventListener("load", () => {
-                var base64 = document.getElementById("imgFondoBase64").value;
-                document.getElementById("contFondo").style.backgroundImage = `url(data:image/png;base64,${base64})`;
-            });
-
-        </script>
+        
+        <script src="js/main.js"></script>
 
     </body>
 </html>
